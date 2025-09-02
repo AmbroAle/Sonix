3e8858c2ba81f922cb9242778f8a8ea4c3c82240
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2025-09-02 14:59:24 +0200

    Fixed notify in homeView

--- Spotify/UI/HomeView.swift
+++ Spotify/UI/HomeView.swift
@@ -8,47 +8,16 @@ struct HomeView: View {
     @EnvironmentObject var notificationManager: NotificationManager
     @EnvironmentObject var appViewModel: AppViewModel
 
-
     var body: some View {
         NavigationStack {
-            ZStack {
-                VStack(spacing: 10) {
-                    TopBarView(selectedTab: $selectedTab, profileViewModel: profileVM, showNotification: showInAppNotification)
-                        .frame(maxWidth: .infinity)
-                        .environmentObject(appViewModel)
-                    contentView(for: selectedTab)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                }
-                .background(.ultraThinMaterial)
-                
-                if let message = notificationManager.inAppMessage {
-                    VStack {
-                        HStack {
-                            Image(systemName: "bell.fill")
-                                .foregroundColor(.white)
-                            Text(message)
-                                .foregroundColor(.white)
-                                .fontWeight(.medium)
-                            Spacer()
-                            Button {
-                                notificationManager.inAppMessage = nil
-                            } label: {
-                                Image(systemName: "xmark")
-                                    .foregroundColor(.white)
-                            }
-                        }
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(12)
-                        .shadow(radius: 5)
-                        .padding(.horizontal)
-                        
-                        Spacer()
-                    }
-                    .transition(.move(edge: .top))
-                    .animation(.easeInOut, value: notificationManager.inAppMessage)
-                }
+            VStack(spacing: 10) {
+                TopBarView(selectedTab: $selectedTab, profileViewModel: profileVM, showNotification: showInAppNotification)
+                    .frame(maxWidth: .infinity)
+                    .environmentObject(appViewModel)
+                contentView(for: selectedTab)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
+            .background(.ultraThinMaterial)
             .environmentObject(notificationManager)
         }
         .navigationBarBackButtonHidden(true)
@@ -63,7 +32,6 @@ struct HomeView: View {
         guard inAppEnabled else { return }
         notificationManager.show(message: message)
     }
-
 
     @ViewBuilder
     private func contentView(for tab: String) -> some View {
